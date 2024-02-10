e01f6c0f53bd7083814a719b5b288a5fcae13917
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-02-10 11:45:56 +0530

    🌟 Constant Variables

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const USER_AGENT_HEADER =
+  "Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0";
+
+export const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
+
+export const URLs = {
+  BASE: "https://aniwatch.to",
+  HOME: "https://aniwatch.to/home",
+  SEARCH: "https://aniwatch.to/search",
+  GENRE: "https://aniwatch.to/genre",
+};
+
+export const ACCEPT_HEADER =
+  "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8";
