43bab3a4d9d9a41f77d86e5c45e3616d375c3b83
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-02-11 14:07:11 +0530

    🎮 Home Controller️

---
+++ src/controllers/aniwatch/homeController.ts
@@ -0,0 +1,15 @@
+import { scrapeHomePage } from "../../scrapers/aniwatch/scrapers";
+import type { RequestHandler } from "express";
+
+const getHomeInfo: RequestHandler = async (_req, res) => {
+  try {
+    const data = await scrapeHomePage();
+    res.status(200).json(data);
+  } catch (err) {
+    ////////////////////////////////////
+    console.log(err); // for TESTING//
+    ////////////////////////////////////
+  }
+};
+
+export { getHomeInfo };
