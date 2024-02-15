ec3214f79b0ada2a0e0060984720c49b08748ff1
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-02-15 16:57:10 +0530

    🐞 Episodes Details Bug Fixed

--- src/extracters/aniwatch/mostpopular_animes.ts
+++ src/extracters/aniwatch/mostpopular_animes.ts
@@ -1,7 +1,6 @@
 import type { CheerioAPI, SelectorType } from "cheerio";
 import createHttpError from "http-errors";
 import { AxiosError } from "axios";
-import { extract_related_animes } from "./extracters";
 import { MostPopularAnime } from "../../types/aniwatch/anime";
 
 export const extract_mostpopular_animes = (
@@ -9,7 +8,65 @@ export const extract_mostpopular_animes = (
   selectors: SelectorType,
 ): MostPopularAnime[] => {
   try {
-    const animes: MostPopularAnime[] = extract_related_animes($, selectors);
+    const animes: MostPopularAnime[] = [];
+
+    $(selectors).each((_index, element) => {
+      const animeID =
+        $(element)
+          .find(".film-detail .dynamic-name")
+          ?.attr("href")
+          ?.slice(1)
+          .trim() || null;
+      const animeNAME =
+        $(element).find(".film-detail .dynamic-name")?.text()?.trim() ??
+        "UNKNOWN ANIME";
+      const animeIMG =
+        $(element)
+          .find(".film-poster .film-poster-img")
+          ?.attr("data-src")
+          ?.trim() || null;
+      const epSUB =
+        Number(
+          $(element)
+            .find(".fd-infor .tick .tick-item.tick-sub")
+            ?.text()
+            ?.trim(),
+        ) || null;
+      const epDUB =
+        Number(
+          $(element)
+            .find(".fd-infor .tick .tick-item.tick-dub")
+            ?.text()
+            ?.trim(),
+        ) || null;
+      const total_eps =
+        Number(
+          $(element)
+            .find(".fd-infor .tick .tick-item.tick-eps")
+            ?.text()
+            ?.trim(),
+        ) || null;
+      const animeTYPE =
+        $(selectors)
+          ?.find(".fd-infor .tick")
+          ?.text()
+          ?.trim()
+          ?.replace(/[\s\n]+/g, " ")
+          ?.split(" ")
+          ?.pop() || null;
+
+      animes.push({
+        id: animeID,
+        name: animeNAME,
+        category: animeTYPE,
+        img: animeIMG,
+        episodes: {
+          eps: total_eps,
+          sub: epSUB,
+          dub: epDUB,
+        },
+      });
+    });
 
     return animes;
   } catch (err) {
