32c9f91f5f11dfc2f58ce14b54883694578802d9
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-02-19 11:31:28 +0530

    📰 Global Lib isSiteReachable

---
+++ src/lib/isSiteReachable.ts
@@ -0,0 +1,8 @@
+export const isSiteReachable = async (url: string): Promise<boolean> => {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+}
