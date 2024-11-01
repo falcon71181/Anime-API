8aca63431832335e0cbaf4c91c7e427a1a0d1428
falcon71181 <abhaythakur71181@gmail.com>
2024-11-01 22:57:34 +0530

    feat: abstract class video extractor

---
+++ src/utils/aniwatch/video-extractor.ts
@@ -0,0 +1,26 @@
+import { IVideo, ISource } from "../../types/aniwatch/anime";
+import Proxy from "./proxy";
+
+abstract class VideoExtractor extends Proxy {
+  /**
+   * The server name of the video provider
+   */
+  protected abstract serverName: string;
+
+  /**
+   * list of videos available
+   */
+  protected abstract sources: IVideo[];
+
+  /**
+   * takes video link
+   *
+   * returns video sources (video links) available
+   */
+  protected abstract extract(
+    videoUrl: URL,
+    ...args: any
+  ): Promise<IVideo[] | ISource>;
+}
+
+export default VideoExtractor;
