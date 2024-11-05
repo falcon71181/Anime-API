63ce2e29a3969edc7a0903ed9bdab0ace3dd48a0
falcon71181 <abhaythakur71181@gmail.com>
2024-11-05 08:18:51 +0530

    feat: added raw episode streaming links

--- src/extracters/aniwatch/server_id.ts
+++ src/extracters/aniwatch/server_id.ts
@@ -3,8 +3,10 @@ import type { CheerioAPI } from "cheerio";
 export const extract_server_id = (
   $: CheerioAPI,
   index: number,
-  category: "sub" | "dub",
+  category: "sub" | "dub" | "raw",
 ) => {
+  console.warn("DEBUGPRINT[2]: server_id.ts:5: index=", index);
+  console.warn("DEBUGPRINT[3]: server_id.ts:6: category=", category);
   return (
     $(`.ps_-block.ps_-block-sub.servers-${category} > .ps__-list .server-item`)
       ?.map((_, el) =>
