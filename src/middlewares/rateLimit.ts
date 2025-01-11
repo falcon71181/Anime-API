b9b0fbffb81aa996bcfc483dc28519f0f8b4592f
Saif Ur Rehman <76581725+S-Tech04@users.noreply.github.com>
2025-01-11 20:08:35 +0500

    Increase requests rate limit to 1500
    Increase the request rate limit to 1500 requests per hour.
    
    * Update the `max` property in `src/middlewares/rateLimit.ts` from 50 to 1500.
    * Ensure the `message` property remains the same.

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1500, // Limit each IP to 1500 requests per `window` (here, per 60 minutes)
   standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy`` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message:
