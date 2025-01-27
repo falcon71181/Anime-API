67d3539d66c12e91a4a0384274c3ed2d78e4a543
Saif Ur Rehman <76581725+S-Tech04@users.noreply.github.com>
2025-01-27 22:27:59 +0500

    Implement cache system and update rate limit

--- src/routes/aniwatch/routes.ts
+++ src/routes/aniwatch/routes.ts
@@ -9,31 +9,32 @@ import {
   getAnimeEpisodeSourcesInfo,
   getatozPage,
 } from "../../controllers/aniwatch/controllers";
+import cacheMiddleware from "../../middlewares/cache";
 
 const aniwatch_router: IRouter = Router();
 
 // /aniwatch/
-aniwatch_router.get("/", getHomePageInfo);
+aniwatch_router.get("/", cacheMiddleware, getHomePageInfo);
 
 // aniwatch/az-list
-aniwatch_router.get("/az-list", getatozPage);
+aniwatch_router.get("/az-list", cacheMiddleware, getatozPage);
 
 // /aniwatch/search?keyword=$(query)&page=$(page)
-aniwatch_router.get("/search", getSearchPageInfo);
+aniwatch_router.get("/search", cacheMiddleware, getSearchPageInfo);
 
 // /aniwatch/anime/:id
-aniwatch_router.get("/anime/:id", getAboutPageInfo);
+aniwatch_router.get("/anime/:id", cacheMiddleware, getAboutPageInfo);
 
 // /aniwatch/episodes/:id
-aniwatch_router.get("/episodes/:id", getEpisodesInfo);
+aniwatch_router.get("/episodes/:id", cacheMiddleware, getEpisodesInfo);
 
 // /aniwatch/servers?id=${id}
-aniwatch_router.get("/servers", getEpisodeServersInfo);
+aniwatch_router.get("/servers", cacheMiddleware, getEpisodeServersInfo);
 
 // /aniwatch/episode-srcs?id=${episodeId}?server=${server}&category=${category (dub or sub)}
-aniwatch_router.get("/episode-srcs", getAnimeEpisodeSourcesInfo);
+aniwatch_router.get("/episode-srcs", cacheMiddleware, getAnimeEpisodeSourcesInfo);
 
 //  aniwatch/:category?page=${page}
-aniwatch_router.get("/:category", getCategoryPage);
+aniwatch_router.get("/:category", cacheMiddleware, getCategoryPage);
 
 export default aniwatch_router;
